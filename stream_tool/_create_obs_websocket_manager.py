5b0742da5f6696c20ce20d51bfb8b71e7bb0d14a
skylerficklin <skylerficklin@gmail.com>
2023-11-20 23:22:31 -0500

    fixed an issue where _create_obs_websocket_manager.py function create_obs_websocket_manager() was using the default value for the authentication info from Websosckets_Auth instead of importing a reference to Websockets_Auth

--- stream_tool/_create_obs_websocket_manager.py
+++ stream_tool/_create_obs_websocket_manager.py
@@ -1,10 +1,10 @@
 from obswebsocket import obsws  # noqa: E402
-from .Websockets_Auth import websocket_host, websocket_port, websocket_password
+from . import Websockets_Auth
 
 
 def create_obs_websocket_manager():
     # Connect to websockets
-    ws = obsws(websocket_host, websocket_port, websocket_password)
+    ws = obsws(Websockets_Auth.websocket_host, Websockets_Auth.websocket_port, Websockets_Auth.websocket_password)
     ws.connect()
     print("Connected to OBS Websockets!\n")
     return ws
